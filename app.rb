85cedced2a816f422dde399b310421218a9840c6
Joseph Cho <jscho13@gmail.com>
2019-02-10 23:39:02 -0500

    Add dotenv

--- app.rb
+++ app.rb
@@ -12,22 +12,22 @@ get '/index' do
   uri = URI.parse('https://www.googleapis.com/books/v1/volumes')
   @curr_idx = params['startIndex'].to_i
   @query = params['q']
-  @key = 'AIzaSyB4dnWKZafl6wa88vvZGss2r-p_LjrV_0k'
-  q_params = { q: @query, startIndex: @curr_idx, key: @key }
+  q_params = { q: @query, startIndex: @curr_idx, key: ENV['API_KEY'] }
   uri.query = URI.encode_www_form( q_params )
 
   # Handle response
   begin
+    puts uri
     response = Net::HTTP.get(uri)
     response = JSON.parse(response)
 
-    # Currently GoogleBooks doesn't reliably provide pagination
-    # total_items = response['totalItems']
-    # @max_page = total_items/10-1
-    # @max_page += 1 if total_items%10 > 0
+    # GoogleBooks doesn't reliably provide pages only volume ID's 
+    total_items = response['totalItems']
+    @max_page = total_items/10-1
+    @max_page += 1 if total_items%10 > 0
 
-    p_str = "/index?q=#{@query}&startIndex=#{@curr_idx-1}&key#{@key}"
-    n_str = "/index?q=#{@query}&startIndex=#{@curr_idx+1}&key#{@key}"
+    p_str = "/index?q=#{@query}&startIndex=#{@curr_idx-1}&key=#{ENV['API_KEY']}"
+    n_str = "/index?q=#{@query}&startIndex=#{@curr_idx+1}&key=#{ENV['API_KEY']}"
     @prev_url = URI::encode(p_str)
     @next_url = URI::encode(n_str)
 
