d9485e0730016259d961c0d8ef49a18ee0ee8d50
Joseph Cho <jscho13@gmail.com>
2019-02-10 23:16:11 -0500

    Add normalize.css, skeleton.css, and prev/next buttons

--- app.rb
+++ app.rb
@@ -1,23 +1,40 @@
 # app.rb
+require 'net/http'
 require 'sinatra'
 require 'uri'
-require 'net/http'
-require 'pry'
 
 get '/' do
   redirect '/index'
 end
 
 get '/index' do
+  # Create query
   uri = URI.parse('https://www.googleapis.com/books/v1/volumes')
-  q_params = { q: params['q'] }
-  uri.query = URI.encode_www_form( q_params  )
+  @curr_idx = params['startIndex'].to_i
+  @query = params['q']
+  q_params = { q: @query, startIndex: @curr_idx }
+  uri.query = URI.encode_www_form( q_params )
 
-  # Parse response
-  response = Net::HTTP.get(uri)
-  response = JSON.parse(response)
-  # TODO: error handling on failed http request
+  # Handle response
+  begin
+    response = Net::HTTP.get(uri)
+    response = JSON.parse(response)
 
-  @books = response['items']
+    # Currently GoogleBooks doesn't reliably provide pagination
+    # total_items = response['totalItems']
+    # @max_page = total_items/10-1
+    # @max_page += 1 if total_items%10 > 0
+
+    p_str = "/index?q=#{@query}&startIndex=#{@curr_idx-1}"
+    n_str = "/index?q=#{@query}&startIndex=#{@curr_idx+1}"
+    @prev_url = URI::encode(p_str)
+    @next_url = URI::encode(n_str)
+
+    @books = response['items']
+    @error = false
+  rescue StandardError
+    @error = true
+  end
+
   erb :index
 end
