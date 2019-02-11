8a23b628497757f4797c1653a23d2b5c4397af00
Joseph Cho <jscho13@gmail.com>
2019-02-10 20:52:04 -0500

    Migrate app from Rails to slimmer Sinatra

--- app.rb
+++ app.rb
@@ -1,8 +1,23 @@
 # app.rb
-require "sinatra"
+require 'sinatra'
+require 'uri'
+require 'net/http'
+require 'pry'
 
-class App < Sinatra::Base
-  get "/" do
-    erb :index
-  end
+get '/' do
+  redirect '/index'
+end
+
+get '/index' do
+  uri = URI.parse('https://www.googleapis.com/books/v1/volumes')
+  q_params = { q: params['q'] }
+  uri.query = URI.encode_www_form( q_params  )
+
+  # Parse response
+  response = Net::HTTP.get(uri)
+  response = JSON.parse(response)
+  # TODO: error handling on failed http request
+
+  @books = response['items']
+  erb :index
 end
